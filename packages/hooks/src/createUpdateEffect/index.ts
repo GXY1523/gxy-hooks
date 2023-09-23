66d804e902c05b99584727ede40a3cd60f6ea116
GXY <gxy252@outlook.com>
2023-09-23 16:47:53 +0800

    modified:   packages/hooks/src/createUpdateEffect/index.ts 	modified:   packages/hooks/src/useUpdateEffect/index.ts

--- packages/hooks/src/createUpdateEffect/index.ts
+++ packages/hooks/src/createUpdateEffect/index.ts
@@ -3,24 +3,26 @@ import type { useEffect, useLayoutEffect } from 'react';
 
 type EffectHookType = typeof useEffect | typeof useLayoutEffect;
 
+// 创建/更新效果
 export const createUpdateEffect: (hook: EffectHookType) => EffectHookType =
   (hook) => (effect, deps) => {
     const isMounted = useRef(false);
 
-    // for react-refresh
+    // 处理react 刷新功能(react-refresh)
+    // 组件卸载时， isMounted.current = false;
     hook(() => {
       return () => {
         isMounted.current = false;
-      };
-    }, []);
+      }
+    },[]);
 
     hook(() => {
-      if (!isMounted.current) {
-        isMounted.current = true;
-      } else {
-        return effect();
+      if(!isMounted.current) {
+        isMounted.current = true
+      }else{
+        return effect()
       }
-    }, deps);
-  };
+    }, deps)
+  }
 
 export default createUpdateEffect;
